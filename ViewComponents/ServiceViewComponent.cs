2cad9d6034e6285ef3231f6b7931aa720f352e1f
muradmecidov <muradmecidov1499@gmail.com>
2023-05-22 11:10:11 +0400

    Taskbasket

---
+++ ViewComponents/ServiceViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebFrontToBack.DAL;
+using WebFrontToBack.Models;
+
+namespace WebFrontToBack.ViewComponents
+{
+    public class ServiceViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public ServiceViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int skip = 0, int take = 8)
+        {
+            List<Service> services = await _context.Services
+                                                 .Where(s => !s.IsDeleted)
+                                                 .OrderByDescending(s => s.Id)
+                                                 .Skip(skip)
+                                                 .Take(take)
+                                                 .Include(s => s.Category)
+                                                 .Include(s => s.ServiceImages)
+                                                 .ToListAsync();
+
+            return View(services);
+        }
+    }
+}
---
+++ ViewModel/BasketItemVM.cs
@@ -0,0 +1,13 @@
+﻿namespace WebFrontToBack.ViewModel
+{
+    public class BasketItemVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public double Price { get; set; }
+        public string CategoryName { get; set; }
+        public string ImagePath { get; set; }
+        public int ServiceCount { get; set; }
+    }
+}
